37f946e2e026e2cb03d363d3089018b4c04ab60f
Gina Vargas <gina.vargas89@outlook.com>
2021-04-05 20:36:49 -0700

    added content to models and server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+//needs express, mongoose require
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.envPORT || 3000;
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+const URI = process.env.MOGODB_URI || "mongodb://localhost/workout"
+mongoose.connect(URI, {
+    useUnifiedTopology: true,
+    useNewUrlParserL: true,
+    useFindAndModify: false
+});
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}!`)
+});
